75e8daf47e0986500f141cc755246ab434eb9757
darwin808 <you@domain.com>
2021-01-31 10:09:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 function App() {
+  const getQuery = gql`
+    query MyQuery {
+      hasura1 {
+        name
+        id
+      }
+    }
+  `;
+
+  const add_Data = gql`
+    mutation MyMutation($name: String) {
+      insert_hasura1(objects: { name: $name }) {
+        returning {
+          name
+          id
+        }
+      }
+    }
+  `;
+
+  const del_data = gql`
+    mutation MyMutation($id: uuid) {
+      delete_hasura1(where: { id: { _eq: $id } }) {
+        returning {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const update_data = gql`
+    mutation MyMutation($idholder: uuid, $edit: String) {
+      update_hasura1(where: { id: { _eq: $idholder } }, _set: { name: $edit }) {
+        returning {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { data, loading } = useQuery(getQuery);
+  const [addData] = useMutation(add_Data);
+  const [delData] = useMutation(del_data);
+  const [updateData] = useMutation(update_data);
+  const [name, setname] = useState("");
+  const [edit, setedit] = useState("");
+  const [modal, setmodal] = useState(false);
+  const [idholder, setidholder] = useState(0);
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    addData({ variables: { name } });
+  };
+
+  const deleteDataOnclick = (id) => {
+    delData({ variables: { id } });
+  };
+
+  const openmodal = (id) => {
+    setidholder(id);
+    setmodal(true);
+  };
+
+  const handleedit = (e) => {
+    e.preventDefault();
+    updateData({ variables: { idholder, edit } });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="submit" onSubmit={handlesubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+      </form>
+
+      {modal && (
+        <form action="submit" onSubmit={handleedit}>
+          <input
+            type="text"
+            value={edit}
+            onChange={(e) => {
+              setedit(e.target.value);
+            }}
+          />
+        </form>
+      )}
+
+      {loading ? (
+        <h1>Please wait....</h1>
+      ) : (
+        <div>
+          {data.hasura1.map((e) => (
+            <div key={e.id}>
+              {e.name}{" "}
+              <button
+                onClick={() => {
+                  deleteDataOnclick(e.id);
+                }}>
+                delete
+              </button>
+              <button
+                onClick={() => {
+                  openmodal(e.id);
+                }}>
+                edit
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://factual-falcon-21.hasura.app/v1/graphql",
+  }),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
